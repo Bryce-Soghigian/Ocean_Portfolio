36c61ece720161037dfe1d0d3890589d1fb50c51
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-10-12 15:40:23 -0500

    my boy swimming

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from 'styled-components';
 import oceanVid from './Ocean.mp4'
+const VideoDiv = styled.div`
+height: 100%;
+width: 100%;
+float: left;
+top: 0;
+padding: none;
+position: fixed; 
+
+`
 export default function Intro() {
     return (
-        <div>
-            <video src={oceanVid}/>
-        </div>
+        <VideoDiv>
+            <video src={oceanVid} type="video/mp4" loop autoPlay/>
+        </VideoDiv>
     )
 }
